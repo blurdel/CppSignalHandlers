a3892f7f8d32272704b025db1f0ce86c3179a071
blurdel <blurdel@gmail.com>
2022-02-15 11:22:44 -0500

    Added SIGUSR1 to close terminal window on timeout

--- src/SignalHandlers.cpp
+++ src/SignalHandlers.cpp
@@ -3,9 +3,12 @@
 #include <iostream>
 #include <thread>
 #include <signal.h>
+#include <unistd.h>
+#include <wait.h>
 
 
 bool running = true;
+int count = 8;
 
 // Catch CTRL+C to tell application to shutdown
 inline void stopHandler(int)
@@ -14,10 +17,16 @@ inline void stopHandler(int)
 	std::cout << "Got signal to shutdown." << std::endl;
 }
 
+inline void userHandler(int sig)
+{
+	std::cout << "Got user signal " << sig << ", count = " << count << std::endl;
+}
+
 inline void setupSignalHandlers()
 {
 	signal(SIGINT,  stopHandler);
 	signal(SIGTERM, stopHandler);
+	signal(SIGUSR1, userHandler); // USR
 }
 
 int main()
@@ -28,7 +37,12 @@ int main()
 
 	while (running) {
 		std::this_thread::sleep_for(std::chrono::seconds(1));
-		std::cout << "running ..." << std::endl;
+		std::cout << "or quitting in " << count << " seconds ..." << std::endl;
+
+		if (--count == 0) {
+			pid_t parent = getppid();
+			kill(parent, SIGUSR1);
+		}
 	}
 
 	std::cout << "Done." << std::endl;
